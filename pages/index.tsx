f3c3f9749c614805218a798192d9f24ea64066af
OliverMoscow <56978722+OliverMoscow@users.noreply.github.com>
2022-05-12 00:17:03 -0400

    experimenting with pouchdb

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,40 +1,67 @@
-
-import Router from 'next/router'
+import Router from "next/router";
 import React, { useState } from "react";
+import PouchDB from "pouchdb";
+
+//commands to kill couch db
+// sudo lsof -i :5984
+//kill "PID"
 
 export default function Home() {
   const [email, setEmail] = useState("");
-  
+
   //@ts-ignore
   const handleSubmit = async (e) => {
     e.preventDefault();
-      const domain = "http://" + window.location.host 
+    var db = new PouchDB('http://localhost:5984/kittens');
+    var doc = {
+      "_id": "mittens",
+      "name": "Mittens",
+      "occupation": "kitten",
+      "age": 3,
+      "hobbies": [
+        "playing with balls of yarn",
+        "chasing laser pointers",
+        "lookin' hella cute"
+      ]
+    };
+    // db.put(doc);
+    db.get('mittens').then(function (doc) {
+      console.log(doc);
+    });
 
-      const res = await fetch("/api/sendgrid", {
-        body: JSON.stringify({ "email": email, "callback": domain + '/siwe' }),
-        headers: {
-          "Content-Type": "application/json",
-        },
-        method: "POST",
-      });
+    // const domain = "http://" + window.location.host;
 
-      const { error } = await res.json();
-      if (error) {
-        console.log(error);
-        return;
-      }
-      Router.push(domain + "/emailVerification")
+    // const res = await fetch("/api/sendgrid", {
+    //   body: JSON.stringify({ email: email, callback: domain + "/siwe" }),
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   method: "POST",
+    // });
+
+    // const { error } = await res.json();
+    // if (error) {
+    //   console.log(error);
+    //   return;
+    // }
+
+    // Router.push(domain + "/emailVerification");
   };
 
-  
   return (
     <div className="container">
-        <form onSubmit={handleSubmit}>
-          <h3> Patient </h3>
-          <label htmlFor="email">sign in via email</label>
-          <input type="email" name="email" value={email} onChange={e => setEmail(e.target.value)} required />
-          <button type="submit">submit</button>
-        </form>
+      <form onSubmit={handleSubmit}>
+        <h3> Patient </h3>
+        <label htmlFor="email">sign in via email</label>
+        <input
+          type="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <button type="submit">submit</button>
+      </form>
     </div>
   );
 }
