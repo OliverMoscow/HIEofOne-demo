2460225cb7fc92b65766adc0705b91cf06723c4d
OliverMoscow <56978722+OliverMoscow@users.noreply.github.com>
2022-05-13 11:53:07 -0400

    small changes

--- components/header.tsx
+++ components/header.tsx
@@ -1,14 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Link from "next/link";
 
 export default class Header extends Component {
-  render () {
+  render() {
     return (
-        <div className="header">
-            <h1>HEI of One</h1>
-            <h5>HIE of One Trustee Community</h5>
-            <a>Privacy Policy</a>
-            <a>Support</a>
-        </div>
+      <div className="header">
+        <h1>HEI of One</h1>
+        <h5>HIE of One Trustee Community</h5>
+        <Link href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley">
+          <a>Privacy Policy</a>
+        </Link>
+        <Link href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley">
+          <a>Support</a>
+        </Link>
+      </div>
     );
   }
 }
