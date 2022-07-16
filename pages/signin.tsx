bdd3c4a2edd2b13f50c2b0433dd4683711e80442
OliverMoscow <56978722+OliverMoscow@users.noreply.github.com>
2022-07-16 14:52:06 -0400

    prep for magic link

---
+++ pages/signin.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+
+import { Magic } from 'magic-sdk';
+
+const m = new Magic('pk_live_9DBB599DA424C04B'); // ✨
+
+
+
+//Landing Page
+const Home = () => {
+  
+  return (
+    <div>
+    </div>
+  );
+};
+
+export default Home;
